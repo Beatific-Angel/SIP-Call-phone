b83a75be11e070803c34db8b8e551756ad7cce64
Beatific-angel <64799935+Beatific-Angel@users.noreply.github.com>
2022-02-03 06:01:47 +0300

    update
    update

--- components/sip_client/include/boost/sml.hpp
+++ components/sip_client/include/boost/sml.hpp
@@ -1,9 +1,7 @@
-//
-// Copyright (c) 2016-2020 Kris Jusiak (kris at jusiak dot net)
-//
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
+/*
+   Copyright Beatific Angel
+   
+ */
 #ifndef BOOST_SML_HPP
 #define BOOST_SML_HPP
 #if (__cplusplus < 201305L && _MSC_VER < 1900)
